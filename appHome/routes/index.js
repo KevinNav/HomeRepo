8946573eeb1621f5e71a236302d8c40da8481013
Kevin_Nav <kvin11@hotmail.com>
2016-05-31 23:54:49 -0600

    New Modification!
    layout.hbs & index.js

--- appHome/routes/index.js
+++ appHome/routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.use(function(req,res,next){
+  res.locals.project = "Mi App Start Page";
+  next();
 });
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Kevin' });
+});
+
 
 module.exports = router;
